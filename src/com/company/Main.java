552b7995b7688c390fd0c63bc1b4a37e84bb0c01
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2020-12-03 21:24:23 +0400

    IF ELSE

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hjhjfgfggffgfgfgf");
+        int x = 7;
+        int y = 13;
+        int c = 48;
+    if(x<y){
+        System.out.println("Gago");
+        if(x<c){
+            System.out.println("x<c");
+
+        }
+    }else{
+        System.out.println("Shiraz");
     }
+
+}
 }
