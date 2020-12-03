88786417c274a0bdd7c79f72bce31abef2a817fa
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2020-12-03 22:39:36 +0400

    hth

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,45 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-        int x = 7;
-        int y = 13;
-        int c = 48;
-    if(x<y){
-        System.out.println("Gago");
-        if(x<c){
-            System.out.println("x<c");
+        String a = "BAREV TIGRAN";
+        System.out.println(a.length());
 
+        int x = 2;
+        int y = 1;
+        int z = 18;
+        if (x > y && x > z) {
+            System.out.println("x max");
+            if (y > z) {
+                System.out.println("y");
+                System.out.println("z");
+            } else {
+                System.out.println("z");
+                System.out.println("y");
+            }
         }
-    }else{
-        System.out.println("Shiraz");
+        if (y > x && y > z) {
+            System.out.println("y max");
+            if (x > z) {
+                System.out.println("x");
+                System.out.println("z");
+            } else {
+                System.out.println("z");
+                System.out.println("x");
+            }
+        }
+        if (z > x && z > y) {
+            System.out.println("z max");
+            if (x > y) {
+                System.out.println("x");
+                System.out.println("y");
+            } else {
+                System.out.println("y");
+                System.out.println("x");
+            }
+        }
+
     }
 
 }
-}
+
