dcc7437c37d64765edc8f488f03a5f93dfdc317c
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2020-12-04 21:47:50 +0400

    LessonTwo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int x = -13;
+        int y;
+
+        if (x > 0) {
+            y = 2 * x - 10;
+            System.out.println("y=" + y);
+        }
+        if (x == 0) {
+            y=x;
+            System.out.println("y="+y);
+
+        }
+        if(x<0){
+            y=-x*2-1;
+            System.out.println("y="+y);
+        }
+
+
     }
 }
