5af0ce5449651fb9498edb6a7eeebe7575c8958d
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2020-12-04 20:42:43 +0400

    LessonOneExOne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("write number");
+        int x = scanner.nextInt();
+        int b = 0;
+        int c;
+        int a;
+        if (x > 0) {
+            b = x + 1;
+            System.out.println(" " + b);
+        }
+        if (x < 0) {
+            c = x - 2;
+            System.out.println(" " + c);
+        }
+        if (x == 0) {
+            a = x + 10;
+            System.out.println(" " + a);
+        }
     }
+
 }
