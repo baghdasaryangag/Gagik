c74b814d951f7e18b84bed316216c25f9f81d47a
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2020-12-06 20:57:35 +0400

    LessonOneExThree

---
+++ src/com/company/LessonOneExThree.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class LessonOneExThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("kord x");
+        int x = scanner.nextInt();
+        Scanner scannertwo = new Scanner(System.in);
+        System.out.println("kord y");
+        int y = scannertwo.nextInt();
+        if (x =>-1 & x =<1 & y =>-1 & y =<1 & x > y);
+        {
+            System.out.println("tru");
+        }
+        else{
+            System.out.println();
+        }
+
+        }
+    }
