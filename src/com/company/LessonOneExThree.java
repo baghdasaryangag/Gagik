ae63d8532b19216626d07f2658f36abe8c379a70
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2020-12-07 12:02:40 +0400

    LessonOneExThreee

--- src/com/company/LessonOneExThree.java
+++ src/com/company/LessonOneExThree.java
@@ -6,17 +6,15 @@ public class LessonOneExThree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("kord x");
-        int x = scanner.nextInt();
+        double x = scanner.nextDouble();
         Scanner scannertwo = new Scanner(System.in);
         System.out.println("kord y");
-        int y = scannertwo.nextInt();
-        if (x =>-1 & x =<1 & y =>-1 & y =<1 & x > y);
-        {
-            System.out.println("tru");
-        }
-        else{
-            System.out.println();
+        double y = scannertwo.nextDouble();
+        if (x >= -1 && x <= 1 && y >= -1 && y <= 1 && x >= y) {
+            System.out.println("The point is on a triangle");
+        } else {
+            System.out.println("The point is not on the triangle");
         }
 
-        }
     }
+}
