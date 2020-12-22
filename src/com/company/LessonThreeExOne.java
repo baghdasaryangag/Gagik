84c3e12bd0ed5ccf202e9812c9c8fdccac00e483
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2020-12-22 20:22:06 +0400

    LessonThreeExOneO

--- src/com/company/LessonThreeExOne.java
+++ src/com/company/LessonThreeExOne.java
@@ -7,29 +7,25 @@ public class LessonThreeExOne {
         Scanner scanner = new Scanner(System.in);
         System.out.println("write length");
         double y = scanner.nextDouble();
-        for (int i = 0; i < 5; i++) {
-            switch (i) {
+
+            switch (0) {
                 case 0:
                     double dm = y / 10;
                     System.out.println("decimeter to meter  " + "=" + dm + "meter");
                 case 1:
                     double km = y * 1000;
                     System.out.println("kilometer to meter  " + "=" + km + "meter");
-                    break;
                 case 2:
                     double mt = y;
                     System.out.println("meter to meter  " + "=" + mt + "meter");
-                    break;
                 case 3:
                     double mm = y / 1000;
                     System.out.println("milimeter to meter  "  + "=" + mm + "meter");
-                    break;
                 case 4:
                     double sm = y / 100;
                     System.out.println("santimeter to meter  " + "=" + sm + "meter");
-                    break;
             }
 
-        }
+
     }
 }
