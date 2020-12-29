3899714c884c5b8293b210f196095b36eadf87c4
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2020-12-29 15:14:46 +0400

    LessonTwoExtraEx202

---
+++ src/com/company/LessonTwoExtraEx202.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class LessonTwoExtraEx202 {
+    public static void main(String[] args) {
+        int num = 665444;
+        int count = 0;
+        int k = 0;
+        int sum = 0;
+        while (num > 1) {
+            k = num % 10;
+            num = (num - k) / 10;
+            sum += k;
+
+        }
+        System.out.println(sum);
+    }
+}
+
