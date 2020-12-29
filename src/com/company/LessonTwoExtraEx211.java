a7c6852b8b6250badbeb3dca9034f6aff4672904
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2020-12-29 17:13:03 +0400

    LessonTwoExtraEx211/1

--- src/com/company/LessonTwoExtraEx211.java
+++ src/com/company/LessonTwoExtraEx211.java
@@ -2,11 +2,17 @@ package com.company;
 
 public class LessonTwoExtraEx211 {
     public static void main(String[] args) {
-        int[] array = {5, 12, 8, 7, 4};
+        int[] array = {5, 12, -8, 7, 4};
         double sum = 0;
+        int countElements=0;
         for (int i = 0; i < array.length; i++) {
-            sum = array[i] + sum;
+            if (array[i]>0) {
+                sum = array[i] + sum;
+                countElements++;
+
+            }
+
         }
-        System.out.println(sum / array.length);
+        System.out.println(sum / countElements);
     }
 }
