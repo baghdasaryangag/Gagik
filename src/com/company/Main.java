6d2fd5689002ea0c963adea26ce7695dd93c8ef5
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2021-01-08 00:21:20 +0400

    LessonFiveExTwo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        String text = "safsf889215 asw4 7";
+        System.out.println(Integer.valueOf('0') + " " + Integer.valueOf('9'));
+        char[] array = text.toCharArray();
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (Integer.valueOf(array[i]) >= 48 && Integer.valueOf(array[i]) <= 57) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
 
     }
 }
