aad42cd554b9dbdc2002f56508aaca4083d669f0
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2021-01-07 23:12:42 +0400

    LessonFourExTwo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] array = new int[8];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10) + 1;
+        }
+        String arrayString = "";
+        for (int i = 0; i < array.length; i++) {
+            arrayString = arrayString + array[i];
+        }
+        System.out.println(arrayString);
+        for (int i = 1; i < array.length; i = i + 2) {
+            array[i] = 0;
 
+        }
+        for (int element : array) {
+            System.out.print(element + ", ");
+
+        }
     }
 }
