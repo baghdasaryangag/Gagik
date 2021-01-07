ac9dac9809b6ee0fcc1000e4282faa8f2b3751b6
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2021-01-07 19:31:39 +0400

    LessonFourExOne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,51 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int[] arrayOne = new int[10];
+        arrayOne[0] = (int) (Math.random() * 10);
+        arrayOne[1] = (int) (Math.random() * 10);
+        arrayOne[2] = (int) (Math.random() * 10);
+        arrayOne[3] = (int) (Math.random() * 10);
+        arrayOne[4] = (int) (Math.random() * 10);
+        arrayOne[5] = (int) (Math.random() * 10);
+        arrayOne[6] = (int) (Math.random() * 10);
+        arrayOne[7] = (int) (Math.random() * 10);
+        arrayOne[8] = (int) (Math.random() * 10);
+        arrayOne[9] = (int) (Math.random() * 10);
+        maxArray(arrayOne);
+        minArray(arrayOne);
+        averageArray(arrayOne);
+    }
+
+    public static void maxArray(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+
+        }
+        System.out.println("Max: " + max);
+    }
+
+    public static void minArray(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+
+        }
+        System.out.println("Min:" + min);
+    }
+
+    public static void averageArray(int[] array) {
+        double average = 0;
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        double avarage = sum / array.length;
+        System.out.println("Avarage:" + avarage);
     }
 }
