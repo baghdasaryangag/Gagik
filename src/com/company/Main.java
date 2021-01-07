1ba479eb4b59311c0574c841e9f074c6723d9903
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2021-01-07 22:44:37 +0400

    LessonFourExOne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,18 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 
         int[] arrayOne = new int[10];
-        arrayOne[0] = (int) (Math.random() * 10);
-        arrayOne[1] = (int) (Math.random() * 10);
-        arrayOne[2] = (int) (Math.random() * 10);
-        arrayOne[3] = (int) (Math.random() * 10);
-        arrayOne[4] = (int) (Math.random() * 10);
-        arrayOne[5] = (int) (Math.random() * 10);
-        arrayOne[6] = (int) (Math.random() * 10);
-        arrayOne[7] = (int) (Math.random() * 10);
-        arrayOne[8] = (int) (Math.random() * 10);
-        arrayOne[9] = (int) (Math.random() * 10);
+        Random rd = new Random();
+
+        for (int i = 0; i < arrayOne.length; i++) {
+            arrayOne[i] = rd.nextInt(10);
+        }
+
         maxArray(arrayOne);
         minArray(arrayOne);
         averageArray(arrayOne);
@@ -22,7 +20,7 @@ public class Main {
 
     public static void maxArray(int[] array) {
         int max = array[0];
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 1; i < array.length; i++) {
             if (max < array[i]) {
                 max = array[i];
             }
@@ -33,7 +31,7 @@ public class Main {
 
     public static void minArray(int[] array) {
         int min = array[0];
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 1; i < array.length; i++) {
             if (min > array[i]) {
                 min = array[i];
             }
@@ -48,7 +46,7 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             sum = sum + array[i];
         }
-        double avarage = sum / array.length;
-        System.out.println("Avarage:" + avarage);
+        average = sum / array.length;
+        System.out.println("Avarage:" + average);
     }
 }
