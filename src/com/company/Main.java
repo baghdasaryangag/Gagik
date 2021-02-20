19e543696ed118e0fac281e32f7fbebabbad1b8b
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2021-02-20 17:14:46 +0400

    LessonSixExstraTwo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int m = 4;
+        int[][] matricName = {{5, 6, 7, 8}, {4, 3, 2, 1}, {10, 11, 12, 13}, {14, 15, 16, 17}};
+        int sum = 0;
+
+        for (int i = m - 1; i >= 0; i--) {
+
+            for (int j = m - 1; j > i; j--) {
+
+
+                if (matricName[i][j] % 2 == 0) {
+                    sum = sum + matricName[i][j];
+
+                }
+
+            }
+        }
+        System.out.println(sum);
     }
 }
