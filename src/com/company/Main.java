9a1db2341aa5067177e629026a7c51fdb23c991b
baghdasaryangag <75438114+baghdasaryangag@users.noreply.github.com>
2021-02-20 11:35:13 +0400

    LessonSix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(68);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(9);
+        numbers.add(10);
+        numbers.add(11);
+        numbers.add(12);
+        numbers.add(13);
+        numbers.add(14);
+        for (int i = 0; i < numbers.size(); i++) {
+            int sum =
+        }
 
     }
+
 }
